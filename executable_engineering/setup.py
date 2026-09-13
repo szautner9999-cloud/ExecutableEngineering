9e5e59af9d1e14dd662ad4e5ce2f403593395e50
Mike Welland <mike@mikewelland.com>
2026-09-12 22:30:52 -0400

    Bump version to 0.1.33

--- executable_engineering/setup.py
+++ executable_engineering/setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="executable_engineering",
-    version="0.1.32",
+    version="0.1.33",
     description="Demonstrative tools for the Executable Engineering textbook",
     long_description=long_description,
     long_description_content_type="text/markdown",
